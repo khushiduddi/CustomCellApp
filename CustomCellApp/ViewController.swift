5ae2716f54ea490be66ef9e0c59f154ca55093ac
khushiduddi <kduddi2@illinois.edu>
2021-06-23 12:08:57 -0700

    Initial Commit

---
+++ CustomCellApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCellApp
+//
+//  Created by Khushi Duddi on 6/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
